1077d979fc7c2fc8c1664a5adda739431c6514c8
AntoineGuenette <anto.guenette@gmail.com>
2026-03-31 12:02:23 -0400

    Add registration and instructions

--- lab3/src/get_trajectory.py
+++ lab3/src/get_trajectory.py
@@ -5,6 +5,7 @@ from lab3.src.phantom_params import *
 from lab3.src.point_selection import get_grid_points, get_ball_points, get_phantom_points
 from lab3.src.reconstruction import get_calib_mat, get_camera_center, triangulate_points
 from lab2.src.registration import compute_registration_transform
+from lab2.src.utils import euler_from_direction
 
 def get_trajectory() -> str:
     """
@@ -23,12 +24,14 @@ def get_trajectory() -> str:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     lab3_dir = os.path.join(script_dir, '..')
     img_dir = os.path.join(lab3_dir, 'images')
+    calib_img_cam1_path = os.path.join(img_dir, 'ex_imgs', 'AimgLb.png')
+    calib_img_cam2_path = os.path.join(img_dir, 'ex_imgs', 'AimgRb.png')
     img_cam1_path = os.path.join(img_dir, 'ex_imgs', 'AimgLb.png')
     img_cam2_path = os.path.join(img_dir, 'ex_imgs', 'AimgRb.png')
 
     # Get camera point
-    pts_grid_cam1 = get_grid_points(img_cam1_path)
-    pts_grid_cam2 = get_grid_points(img_cam2_path)
+    pts_grid_cam1 = get_grid_points(calib_img_cam1_path)
+    pts_grid_cam2 = get_grid_points(calib_img_cam2_path)
 
     # Define world points
     pts_grid_world = np.array(
@@ -51,8 +54,8 @@ def get_trajectory() -> str:
     calib_mat_cam2 = get_calib_mat(pts_grid_cam2, pts_grid_world)
 
     # Get ball points
-    pts_ball_cam1 = get_ball_points(img_cam1_path)
-    pts_ball_cam2 = get_ball_points(img_cam2_path)
+    pts_ball_cam1 = get_ball_points(calib_img_cam1_path)
+    pts_ball_cam2 = get_ball_points(calib_img_cam2_path)
 
     # Get camera positions in world coordinates
     C_cam1 = get_camera_center(calib_mat_cam1, pts_ball_cam1, pts_ball_world)
@@ -65,28 +68,65 @@ def get_trajectory() -> str:
     pts_phantom_cam2 = get_phantom_points(img_cam2_path)
 
     # Triangulate the points to get phantom points in world coordinates
-    pts_world = triangulate_points()
+    pts_world = triangulate_points(C_cam1, pts_phantom_cam1, C_cam2, pts_phantom_cam2)
 
     # Compute the registration transform from phantom to world coordinates
     T_reg = compute_registration_transform(
-        a_1 = 0, # Change when phantom_params.py is written
-        a_2 = 0, # Change when phantom_params.py is written
-        a_3 = 0, # Change when phantom_params.py is written
+        a_1 = box_back_right_phantom,
+        a_2 = box_front_right_phantom,
+        a_3 = box_front_left_phantom,
         b_1 = pts_world[0],
-        b_2 = pts_world[1],
-        b_3 = pts_world[2]
+        b_2 = pts_world[2],
+        b_3 = pts_world[3]
     )
     R_reg = T_reg[0:3, 0:3]
     t_reg = T_reg[0:3, 3]
 
-    # Define important points (obstacle + target)
+    # Register obstacle corners to world coordinates
+    obs_top_right_world = R_reg @ obs_top_right_phantom + t_reg
+    obs_top_left_world = R_reg @ obs_top_left_phantom + t_reg
+    obs_bottom_right_world = R_reg @ obs_bottom_right_phantom + t_reg
+    obs_bottom_left_world = R_reg @ obs_bottom_left_phantom + t_reg
 
-    # Register those points to world coordinates
+    # Register target corners to world coordinates
+    trg_top_right_world = R_reg @ trg_top_right_phantom + t_reg
+    trg_top_left_world = R_reg @ trg_top_left_phantom + t_reg
+    trg_bottom_right_world = R_reg @ trg_bottom_right_phantom + t_reg
+    trg_bottom_left_world = R_reg @ trg_bottom_left_phantom + t_reg
 
+    # Register trajectory points to world coordinates
+    contact_point_world = R_reg @ contact_point_phantom + t_reg
+    deflection_point_world = R_reg @ deflection_point_phantom + t_reg
+    middle_point_world = R_reg @ middle_point_phantom + t_reg
+    starting_point_world = R_reg @ starting_point_phantom + t_reg
+
+    # Compute effector direction
+    direction = middle_point_world - starting_point_world
+    direction = direction / np.linalg.norm(direction)
+    effector_angles = euler_from_direction(direction)
+    
     # Show augmented images (highlight obstacle and target)
 
     # Print directions
-    instructions = ""
+    instructions = f"""
+SetTRF(0,0,71.3,0,0,0)
+SetJointVel(10)
+SetCartLinVel(10)
+SetCartAngVel(15)
+MovePose({starting_point_world[0]:.2f},{starting_point_world[1]:.2f},{starting_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+Delay(3)
+MoveLin({middle_point_world[0]:.2f},{middle_point_world[1]:.2f},{middle_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+MoveLin({contact_point_world[0]:.2f},{contact_point_world[1]:.2f},{contact_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+Delay(1)
+SetCartLinVel(5)
+MoveLin({deflection_point_world[0]:.2f},{deflection_point_world[1]:.2f},{deflection_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+Delay(5)
+MoveLin({contact_point_world[0]:.2f},{contact_point_world[1]:.2f},{contact_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+MoveLin({middle_point_world[0]:.2f},{middle_point_world[1]:.2f},{middle_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+SetCartLinVel(10)
+MoveLin({starting_point_world[0]:.2f},{starting_point_world[1]:.2f},{starting_point_world[2]:.2f},{effector_angles[0]:.2f},{effector_angles[1]:.2f},{effector_angles[2]:.2f})
+MoveJoints(0,0,0,0,0,0)
+        """
 
     return instructions
 
