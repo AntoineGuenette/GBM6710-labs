671b09163f21b4bdd14fa27f3a0b385b5d214e1a
AntoineGuenette <anto.guenette@gmail.com>
2026-04-02 10:57:46 -0400

    First successful trial

--- lab3/src/phantom_params.py
+++ lab3/src/phantom_params.py
@@ -10,25 +10,25 @@ box_front_right_phantom = np.array([67.31, -179.54, -400.03], dtype=float)
 box_front_left_phantom = np.array([-59.12, -179.54, -399.11], dtype=float)
 
 # Obstacle front corners
-obs_top_right_phantom = np.array([50.98, -156.05, -377.81], dtype=float)
-obs_top_left_phantom = np.array([-44.19, -156.05, -377.81], dtype=float)
-obs_bottom_right_phantom = np.array([41.65, -146.19, -377.81], dtype=float)
-obs_bottom_left_phantom = np.array([-36.26, -146.19, -377.81], dtype=float)
+obs_top_right_phantom = np.array([50.98, -146.05, -377.81], dtype=float)
+obs_top_left_phantom = np.array([-44.19, -146.05, -377.81], dtype=float)
+obs_bottom_right_phantom = np.array([43.05, -134.31, -377.81], dtype=float)
+obs_bottom_left_phantom = np.array([-35.79, -134.31, -377.81], dtype=float)
 
 # Target front corners
-trg_top_right_phantom = np.array([10.86, -118.00, -378.27], dtype=float)
-trg_top_left_phantom = np.array([-4.07, -118.00, -378.27], dtype=float)
-trg_bottom_right_phantom = np.array([10.86, -106.26, -377.81], dtype=float)
-trg_bottom_left_phantom = np.array([-4.54, -106.26, -377.81], dtype=float)
+trg_top_right_phantom = np.array([10.86, -108.00, -378.27], dtype=float)
+trg_top_left_phantom = np.array([-4.07, -108.00, -378.27], dtype=float)
+trg_bottom_right_phantom = np.array([10.86, -96.26, -377.81], dtype=float)
+trg_bottom_left_phantom = np.array([-4.54, -96.26, -377.81], dtype=float)
 
 # Contact point
-contact_point_phantom = np.array([3.39, -118.00, -372.00], dtype=float)
+contact_point_phantom = np.array([3.86, -108.47, -365.78], dtype=float)
 
 # Deflection point (maximum bending of target)
-deflection_point_phantom = np.array([3.39, -113.00, -372.00], dtype=float)
+deflection_point_phantom = np.array([3.86, -102.47, -365.78], dtype=float)
 
 # Middle point (between target and obstacle, inside the phantom)
-middle_point_phantom = np.array([3.39, -132.00, -372.00], dtype=float)
+middle_point_phantom = np.array([3.86, -121.39, -365.78], dtype=float)
 
 # Starting point (between target and obstacle, outside the phantom)
-starting_point_phantom = np.array([3.39, -146.00, -425.00], dtype=float)
+starting_point_phantom = np.array([3.86, -136.00, -425.00], dtype=float)
